601cb08bf61fe6170d73927aa9bf4ce43986d0f5
Garry Mao <smao7434@uni.sydney.au>
2020-06-23 14:13:10 +0800

    g6

---
+++ visual_bbox.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import torch
+import torchvision
+img_name = '00013993_083.png'
+df = pd.read_csv("D:\\X\\2019S2\\3912\\CXR8\\BBox_List_2017.csv")
+box = df.loc[df['Image Index'] == img_name].values[0]
+box = box[2:6].astype(int)
+print(box.shape)
+print(box)
+# print(box)
+# x = box[0]
+# y = box[1]
+# w = box[2]
+# h = box[3]
+boxed = torch.zeros(1024, 1024)
+boxed[box[1] : (box[1] + box[3]), box[0] : (box[0] + box[2])] = 1
+torchvision.utils.save_image(boxed, '/Users/LULU/MILNet/vis/' + img_name)
